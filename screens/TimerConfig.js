773c7919ab6b962775d57ba968fff4d427f5d0ff
rbaker1 <robele.baker@alumni.ubc.ca>
2019-10-27 12:31:32 -0700

    Separating TimerConfig to screen. Folder management

---
+++ screens/TimerConfig.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+//TimerConfig is the screen that the user will set workout timers
+//This is not where the timer will run
+export default function TimerConfig() {
+
+
+  return (
+    <View style={styles.container}>
+      <Text>I am a placeholder until I have functionality</Text>
+    </View>
+  );
