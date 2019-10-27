dedf3033b893817b2f0af0f7604cd6ad469b4cb0
rbaker1 <robele.baker@alumni.ubc.ca>
2019-10-27 16:45:57 -0700

    working on timer functionality. will merge when ready

--- app/components/Timer.js
+++ app/components/Timer.js
@@ -1,5 +1,24 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import {View } from 'react-native';
 
-class Timer {
+class Timer extends Component {
   //do something
+
+  static propTypes = {
+   start: PropTypes.bool,
+   reset: PropTypes.bool,
+   options: PropTypes.object,
+   handleFinish: PropTypes.func,
+   totalDuration: PropTypes.numb er,
+   getTime: PropTypes.func,
+ }
+
+/*
+
+*/
+  render() {
+
+
+  }
 }
