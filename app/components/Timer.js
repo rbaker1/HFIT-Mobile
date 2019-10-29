a2f8eca237e857bde8fa5cc11bee84fba4c4284d
rbaker1 <robele.baker@alumni.ubc.ca>
2019-10-28 18:58:22 -0700

    building skeleton of timer class - does not compile yet, functions do not run.

--- app/components/Timer.js
+++ app/components/Timer.js
@@ -3,17 +3,92 @@ import PropTypes from 'prop-types';
 import {View } from 'react-native';
 
 class Timer extends Component {
-  //do something
-
+  // create a timer that can be initalized, set, ran and destroyed seemlessly
+  // CLASS DOES NOT COMPILE YET
   static propTypes = {
    start: PropTypes.bool,
    reset: PropTypes.bool,
    options: PropTypes.object,
    handleFinish: PropTypes.func,
-   totalDuration: PropTypes.numb er,
+   totalDuration: PropTypes.number,
    getTime: PropTypes.func,
  }
 
+constructor(props) {
+super(props);
+this.state = {
+  active: false, //determines if timer is active and ready to start
+  started: false,
+  remainingTime: props.totalDuration,
+};
+
+this.start = this.start.bind(this);
+this.stop = this.stopTimer.bind(this);
+this.reset = this.reset.bind(this);
+this.timeLeft this.timeLeft.bind(this);
+const width = 150;
+this.defaultStyles = {
+  container: {
+    backgroundColor: '#000', // white - play with time later
+    padding: 5, //
+    borderRadius: 4,
+    width: width, // see if 150 is good
+  },
+  text: {
+    fontSize: 16,
+    color: '#FFF',
+    marginLeft: 7,
+  }
+}
+}
+
+
+
+componentDidMount() {
+  if(this.props.start) {
+    this.start();
+  }
+}
+
+formatTime() {
+  //TODO: Handle how much time is left
+  //this will take this.state.timeLeft and output it into starting
+
+  //timeNow will be in milliseconds
+
+  const getTime = this.props;
+  const timeNow = this.state.timeLeft;
+  const formattedTime = formatTimeToString(timeNow); // TODO: formatTimeToString
+
+  getTime === "function" && getTime(formattedTime);
+
+  return forattedTime;
+
+}
+
+createTimer(...){
+  //TODO: handle beginning behavior once timer is created
+
+}
+
+startTimer() {
+  // TODO: Implement timer to start counting down
+}
+
+stopTimer() {
+  // TODO:
+  // Tell timer to stop
+}
+
+removeTimer() {
+  //TODO: Implement logic if timer is told to remove
+}
+
+resetTimer(...) {
+  // TODO:
+  // I do not believe this function will be implemented at first.
+
+}
 /*
 
 */
