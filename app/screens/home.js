36059f6f1befd72573a7272555623888b249555b
Ben Groves <benjamin.groves@letgo.com>
2019-11-01 17:13:10 +0100

    initial design and layout

---
+++ app/screens/home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Layout, Text, Button, Icon } from 'react-native-ui-kitten';
+
+export const HomeScreen = () => (
+  <Layout style={styles.container}>
+    <Text style={styles.text} category='h4'>Handsfree Fitness app</Text>
+    <LoginButton />
+  </Layout>
+);
+
+const FacebookIcon = (style) => (
+  <Icon name='facebook' {...style} />
+);
+
+const LoginButton = () => (
+  <Button icon={FacebookIcon}>Login with Facebook</Button>
+);
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    marginVertical: 16
+  },
+});
